3052fc48336f2f78360d0bcf9571d3ba2b2cdf5b
Marcello Chiuminatto <mchiuminatto@gmail.com>
2024-06-12 21:06:46 -0300

    DONE: Flows
    Signed-off-by: Marcello Chiuminatto <mchiuminatto@gmail.com>

--- src/tutorial/repo_info.py
+++ src/tutorial/repo_info.py
@@ -1,17 +1,18 @@
 import httpx
-from prefect import flow
+from prefect import flow, get_run_logger
 
 
 @flow
-def get_repo_info():
-    url = "https://api.github.com/repos/PrefectHQ/prefect"
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    url = f"https://api.github.com/repos/{repo_name}"
     response = httpx.get(url)
     response.raise_for_status()  # raises a error for any status different from 2xx
     repo = response.json()
-    print("PrefectHQ/prefect repository statistics 🤓:")
-    print(f"Stars 🌠 : {repo['stargazers_count']}")
-    print(f"Forks 🍴 : {repo['forks_count']}")
+    logger = get_run_logger()
+    logger.info("%s repository statistics 🤓:", repo_name)
+    logger.info(f"Stars 🌠 : %d", repo["stargazers_count"])
+    logger.info(f"Forks 🍴 : %d", repo["forks_count"])
 
 if __name__ == "__main__":
-    get_repo_info()
+    get_repo_info("mchiuminatto/triple_barrier")
     
